846f4ef1c1c5a7ff1d5c0f901b131bc1786eaf9b
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-30 00:10:47 +0200

    add validation

--- pagination/pagination.js
+++ pagination/pagination.js
@@ -1,12 +1,18 @@
 // Stwórz funkcję paginateArray
 const paginateArray = (dataEntries, settings) => {
-    const { actualPageIdx, entriesOnPage } = settings
-    const firstSelectedElement = (actualPageIdx - 1) * entriesOnPage;
-    const secondSelectedElement = firstSelectedElement + entriesOnPage;
-    const entriesOnSelectedPage = dataEntries.slice(firstSelectedElement, secondSelectedElement);
-    return entriesOnSelectedPage
-
+    const { actualPageId, entriesOnPage } = settings
+    if (actualPageId > 0 && Number.isInteger(actualPageId) && entriesOnPage > 0 && Number.isInteger(entriesOnPage) && dataEntries.length > (actualPageId * entriesOnPage)) {
+        firstElToSlice = Math.round((actualPageId - 1) * entriesOnPage);
+        secondElToSlice = Math.round(firstElToSlice + entriesOnPage);
+        const entriesOnSelectedPage = dataEntries.slice(firstElToSlice, secondElToSlice);
+        return entriesOnSelectedPage
+    } else {
+        const entriesOnSelectedPage = "brak rekordów"
+        return entriesOnSelectedPage
+    }
 }
+
+
 // która przyjmuję array do paginacji dataEntries oraz settings o kluczach { actualPageIdx=9, entriesOnPage=50 }
 // - actualPageIdx to index wybranej strony
 // - entriesOnPage to maksymalna zwracana ilość elementów z dataEntries dla wybranej strony
@@ -19,6 +25,6 @@ const dataEntries = []
 for (let i = 1; i < 1000; i++) {
     dataEntries.push(`zdanie ${i} `)
 }
-console.log(paginateArray(dataEntries, { actualPageIdx: 9, entriesOnPage: 50 }))
+console.log(paginateArray(dataEntries, { actualPageId: 9, entriesOnPage: 50 }))
 
 
